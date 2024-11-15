a2feef4d5478d1672cc53c2d283b618ca6b6562a
Saravanan <sivasaravanan9578@gmail.com>
2024-11-15 13:26:01 +0530

    Use dynamic port with process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ const addressRoutes = require('./routes/addressRoutes');
 app.use('/address', addressRoutes); //Import address Routes
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
